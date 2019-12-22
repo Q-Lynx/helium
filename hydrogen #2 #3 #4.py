c84d839b5ed7b14553605ae7ba3daaaad11ade39
Q-Lynx <59139038+Q-Lynx@users.noreply.github.com>
2019-12-22 22:49:29 +0100

    Update hydrogen #2 #3 #4.py

--- hydrogen #2 #3 #4.py
+++ hydrogen #2 #3 #4.py
@@ -1,39 +1,42 @@
 import random
 #Heads or tails
-#początek jest niżej
 def heads_or_tails():
     rzut = ["head", "tail"]
     wynik = random.choice(rzut)
     print(wynik)
-    convert_temperature()
+
 #Temperature Converter
 def convert_temperature():
     odpowiedz2 = input("Do you want to convert temperature? ")
     if odpowiedz2 == "yes":
         zgoda = input("Celcius to Kelvin or Kelvin to Celcius? ")
-        if zgoda("Celcius to Kelvin"):
-            temperatura_w_celcjuszach = input("Write the value. ")
-            kelvin = temperatura_w_celcjuszach + 273, 15
+        if zgoda =="Celcius to Kelvin":
+            temperatura_w_celcjuszach = int(input("Write the value. "))
+            kelvin = temperatura_w_celcjuszach + 273.15
             print(kelvin)
-        elif zgoda("Kelvin to Celcius"):
-            temperatura_w_kelvinach = input("Write the value. ")
-            celcjusz = temperatura_w_kelvinach - 273, 15
+            input("Fahrenheits? Fahrenheits? Who the fck cares about Fahrenheits?")
+            age_in_seconds()
+        elif zgoda == "Kelvin to Celcius":
+            temperatura_w_kelvinach = int(input("Write the value. "))
+            celcjusz = temperatura_w_kelvinach - 273.15
             print(celcjusz)
-    input("Fahrenheits? Fahrenheits? Who the fck cares about Fahrenheits?")
+            input("Fahrenheits? Fahrenheits? Who the fck cares about Fahrenheits?")
+            age_in_seconds()
     if odpowiedz2 == "no":
         age_in_seconds()
 
 #Age in seconds
 def age_in_seconds():
-    wiek = input("How old are you?")
+    wiek = int(input("How old are you? "))
     sekundy = wiek*365*24*60*60
-    print("No, you're ", sekundy," seconds old")
+    print("No, you're ", sekundy,"seconds old")
 
-#początek
-print("Hello, I need to interrupt you. Sorry, I know that you might be busy but... \n I just need to do my homework.")
+#poczatek 
+print("Hello, I need to interrupt you. Sorry, I know that you might be busy but... \nI just need to do my homework.")
 odpowiedz = input("Do you want to flip a coin? ")
 if odpowiedz == "yes":
     heads_or_tails()
+    convert_temperature()
 if odpowiedz == "no":
     convert_temperature()
 input("Goodbye you old looser")
