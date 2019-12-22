f8a124872904f01c8c850efe8c4ecab1a515aa71
Q-Lynx <59139038+Q-Lynx@users.noreply.github.com>
2019-12-22 19:31:16 +0100

    Update hydrogen #2 #3 #4.py

--- hydrogen #2 #3 #4.py
+++ hydrogen #2 #3 #4.py
@@ -1,4 +1,5 @@
 import random
+#początek programu jest na końcu, tu napisałam najpierw funkcje
 #Heads or tails
 def heads_or_tails():
     rzut = ["head", "tail"]
@@ -28,7 +29,7 @@ def age_in_seconds():
     sekundy = wiek*365*24*60*60
     print("No, you're ", sekundy," seconds old")
 
-
+#Początek
 print("Hello, I need to interrupt you. Sorry, I know that you might be busy but... \n I just need to do my homework.")
 odpowiedz = input("Do you want to flip a coin? ")
 if odpowiedz("yes"):
